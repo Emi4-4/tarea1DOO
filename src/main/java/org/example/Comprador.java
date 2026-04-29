76df0eedc0ccdb780b4200bce4a0f253e478c502
arenisca <vseron2000@gmail.com>
2026-04-29 00:23:08 -0400

    Feat: clase Comprador, recibe moneda, producto y el expendedor

---
+++ src/main/java/org/example/Comprador.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Comprador {
+    private String sonido;
+    private int vuelto=0;
+    public Comprador(Moneda moneda, int cualProducto, Expendedor exp) {
+        Producto p=exp.comprarProducto(moneda,cualProducto);
+        Moneda mon=exp.getVuelto();
+        while (mon!=null){
+            vuelto+= mon.getValor();
+        }
+        if (p!=null){
+            sonido=p.consumir();
+        } else {
+            sonido=null;
+        }
+    }
+    public int cuantoVuelto(){
+        return vuelto;
+    }
+    public String queCompraste() {
+        return sonido;
+    }
+}
