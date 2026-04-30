4e154fdfd410f73f1ecb268950c80d4ff26c14d6
arenisca <vseron2000@gmail.com>
2026-04-30 18:22:41 -0400

    Feat: se creó otro producto de tipo dulce-> Bon o Bon

---
+++ src/main/java/org/example/BonoBon.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class BonoBon extends Dulce{
+    public BonoBon(int serie) {
+        super(serie);
+    }
+
+    @Override
+    public String consumir() {
+        return "mmm bonobon";
+    }
+}
